3245a3d22cbaf47be415e283b21a3c21b79ad4d7
Nick H <nickdog1204@gmail.com>
2022-03-20 17:12:47 +0800

    empty typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,1 @@
-import './App.css';
-import Todos from "./components/Todos";
-import NewTodoItem from "./components/NewTodoItem";
-import TodosContextProvider from "./store/todos-context";
-
-function App() {
-    return (
-        <TodosContextProvider>
-            <NewTodoItem/>
-            <Todos/>
-        </TodosContextProvider>
-    );
-}
-
-export default App;
+import React from "react";
--- src/models/todo.ts
+++
@@ -1,9 +0,0 @@
-export class Todo {
-    id: string;
-
-    constructor(
-        public todoText: string,
-    ) {
-        this.id = new Date().toISOString();
-    }
-}
